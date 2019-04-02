d316b07481e16361727548c7f069d793798e1b5d
yujinWang <1219044051@qq.com>
2019-04-03 00:21:49 +0800

    添加ico图标

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -63,7 +63,8 @@ const webpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: config.build.index,
       template: 'index.html',
-      inject: true,
+			inject: true,
+			favicon: path.resolve('favicon1.ico'),
       minify: {
         removeComments: true,
         collapseWhitespace: true,
